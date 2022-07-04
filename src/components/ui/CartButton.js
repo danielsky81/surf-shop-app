7dbd42926e5143253f4c376321723f256ced2107
danielsky81 <karma0rg4u@gmail.com>
2022-07-04 16:48:04 +0100

    Add animation to cart button

--- src/components/ui/CartButton.js
+++ src/components/ui/CartButton.js
@@ -14,14 +14,28 @@ const CartButton = (props) => {
   }, 0);
 
   const btnRef = useRef();
+  const spanRef = useRef();
+
   useEffect(() => {
     gsap.to(btnRef.current, { x: 0, duration: 1 });
   });
+
+  useEffect(() => {
+    console.log(spanRef.current)
+    gsap.to(spanRef.current, { y: 30, duration: .1 });
+    gsap.to(spanRef.current, { y: 30, opacity: 0, duration: .025, delay: .1 });
+    gsap.to(spanRef.current, { y: -30, opacity: 0, duration: .025, delay: .15 });
+    gsap.to(spanRef.current, {y: 0, opacity: 1, duration: .1, delay: .25 });
+    
+    // gsap.to(spanRef.current, { y: 0, duration: .2, delay: .2 });
+  }, [cartCtx.items])
   
   return (
     <button ref={btnRef} className={`${styles.cart} ${global['teko-font']}`} onClick={props.onClick}>
       <span>{props.name}</span>
-      <span className={styles.badge}>{numberOfCartItems}</span>
+      <span className={styles.badge}>
+        <span ref={spanRef}>{numberOfCartItems}</span>
+      </span>
     </button>
   )
 }
